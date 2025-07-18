7cbcd7a20f326051b9992f8a7276521c82a133b0
lubnakhan-Github-Account <164382966+lubnakhan-Github-Account@users.noreply.github.com>
2025-07-18 15:01:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from mcp.server.fastmcp import FastMCP
 
 mcp= FastMCP(name="hello=mcp", stateless_http=True)
 
+@mcp.tool(name="search_online")
+def search_online(query: str)->str:
+  # TODO: impliment search logic
+ return f"Result for {query}..."
+  
+# Transport -> getstarlette instance 
 mcp_app= mcp.streamable_http_app()
 
 
